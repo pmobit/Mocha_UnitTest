e89e943f520a81313aa839e95887ac8d1036f41d
pmobit <46635561+pmobit@users.noreply.github.com>
2022-03-02 16:38:17 -0600

    Add files via upload

---
+++ myClass.js
@@ -0,0 +1,15 @@
+class MyClass {
+constructor (){
+    console.log("initiate");
+
+}
+    add(arg1, arg2){
+        var result;
+        result = arg1 + arg2;
+        return result;
+
+
+    }
+}
+
+module.exports = MyClass;
---
+++ myClass.spec.js
@@ -0,0 +1,11 @@
+var MyClass = require ("../src/myClass.js");
+var myObj = new MyClass();
+var chai = require ("chai");
+var expect = chai.expect;
+
+describe ("Test suit", function (){
+
+    it("Test the add method", function (){
+        expect(myObj.add(1, 2)).to.be.equal(3);
+    });
+});
